bd1e34d644b88591fbefc31fe38c39f0cce5d43f
igor71 <34439207+igor71@users.noreply.github.com>
2018-08-16 09:28:50 +0300

    Delete gpu_tf_check.py

--- gpu_tf_check.py
+++
@@ -1,7 +0,0 @@
-# Python
-import tensorflow as tf
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session(config=config)
-print(sess.run(hello))
